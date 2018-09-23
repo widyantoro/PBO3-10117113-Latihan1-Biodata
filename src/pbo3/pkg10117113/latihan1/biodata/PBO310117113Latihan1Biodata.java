502e91d3ed138cc37df21f36b6784695af384472
Eka Widyantoro <ekawidyantoro17@gmail.com>
2018-09-23 17:51:46 +0700

    first commit

---
+++ src/pbo3/pkg10117113/latihan1/biodata/PBO310117113Latihan1Biodata.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo3.pkg10117113.latihan1.biodata;
+
+/*
+ * 
+ * @author
+ * Nama     : Eka Widyantoro
+ * Kelas    : IF-3 / PBO3
+ * NIM      : 10117113
+ * Deskripsi Program : Program untuk menampilkan value ke layar
+ */
+public class PBO310117113Latihan1Biodata {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        System.out.println("BIODATA ROCKSTAR" + "\n" + "---------------------");
+        System.out.println("NAMA\t\t: Eka Widyantoro");
+        System.out.println("ALAMAT\t\t: Jl. Dahlia No.5");
+        System.out.println("Tempat Lahir\t: Cilacap");
+        System.out.println("Tanggal Lahir\t: 17 Mei 1999");
+        System.out.println("---------------------");
+        System.out.println("Hobi\t\t: Photography");
+        System.out.println("MAKES\t\t: Nasi Goreng");
+        System.out.println("MIKES\t\t: Es Campur");
+        
+    }
+    
+}
